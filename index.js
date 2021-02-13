762f3647e1820a0d6849c39af6b8f9dd7528f162
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-02-13 14:24:28 -0500

    dice game complete

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+
+var randomImageSource = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll(".img1")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+var image2 = document.querySelectorAll(".img2")[0];
+
+image2.setAttribute("src", randomImageSource2);
